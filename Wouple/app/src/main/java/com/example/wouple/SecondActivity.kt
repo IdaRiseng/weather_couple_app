776b446072e2fcd8204242664091a792cd94c306
EmreRuy <uyar.em.eu@gmail.com>
2023-05-07 19:17:07 +0300

    new

--- Wouple/app/src/main/java/com/example/wouple/SecondActivity.kt
+++ Wouple/app/src/main/java/com/example/wouple/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -13,15 +14,20 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Bottom
 import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.BottomStart
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.Top
 import androidx.compose.ui.Alignment.Companion.TopCenter
+import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -55,10 +61,10 @@ fun SecondCardView(){
                 painter = painterResource(id = R.drawable.rainybackground),
                 contentScale = ContentScale.Crop
             )
-            .padding(40.dp, 30.dp, 40.dp, 30.dp),
+            .padding(30.dp, 30.dp, 30.dp, 30.dp),
     ){
         Box(modifier = Modifier
-            .size(300.dp, 360.dp)
+            .size(450.dp, 260.dp)
             .shadow(elevation = 2.dp, shape = RoundedCornerShape(20.dp))
             .background(Dark20)
         ) {
@@ -67,33 +73,21 @@ fun SecondCardView(){
             val activity = LocalContext.current
             val intent  = Intent(activity, MainActivity::class.java)
             val data = intent.getStringExtra("location")
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center) {
+                Icon(painter = painterResource(id = R.drawable.calendar), contentDescription ="", tint = Corn, modifier = Modifier
+                    .size(30.dp)
+                    .padding(start = 10.dp, top = 10.dp))
 
-            Row(modifier = Modifier
-                .padding(horizontal = 5.dp, vertical = 5.dp)
-                .border(
-                    width = 2.dp,
-                    color = Spiro,
-                    shape = RoundedCornerShape(20.dp)
-                )
-                .fillMaxWidth(1f)
-                .height(50.dp)){
-
-
-                if (data != null) {
-                    Text(modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp),
-                        text = data,
-                        color = Corn,
-                        fontSize = 16.sp,
-                    )
-                }
-
-               if (data == null) {
-                    Text(modifier = Modifier.padding(start = 10.dp, top = 10.dp),
-                        text = "set yout location",
+                Divider(
                     color = Corn,
-                    textAlign = TextAlign.Justify)
-                }
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .padding(top = 30.dp)
+                )
             }
+
+
             }
 }
 }
